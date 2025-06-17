e648cfca334bc44feb5e910ed60a3de8911eabe9
Croilan <79257237+Kifranei@users.noreply.github.com>
2025-06-17 09:13:01 +0800

    修复  在留言页查看留言后点击“你好 $user”后跳转回登录页的问题

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ASP.NET 实现的留言板
 数据库连接字符串自行修改web.config里的Server=，两个都要改；
 数据库请使用MS SQL Server 2022 Develop，环境Visual Studio 2022 Community，提前安装.NET 4.8.1
+
+项目完成的过程中使用了GitHub Copilot(GPT-4.1)，让我省去很多debug的时间，感谢它的帮助。
--- personal.aspx.cs
+++ personal.aspx.cs
@@ -12,6 +12,7 @@ namespace messageboard
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // 只校验Session，不再强制要求URL参数，保证登录后跳转不会掉登录
             if (Session["username"] == null || Session["userid"] == null)
             {
                 Response.Write("<script>alert('请先登录！');window.location='Default.aspx';</script>");
@@ -20,12 +21,8 @@ namespace messageboard
             }
             if (!IsPostBack)
             {
-                string uid = Request.QueryString["uid"] ?? Session["userid"]?.ToString();
-                if (string.IsNullOrEmpty(uid))
-                {
-                    Response.Redirect("login.aspx");
-                    return;
-                }
+                // 优先用Session中的userid
+                string uid = Session["userid"]?.ToString();
                 DBClass db1 = new DBClass();
                 string strsql = "select * from register where id = " + uid;
                 DataTable dt = db1.GetRecords(strsql);
@@ -47,7 +44,8 @@ namespace messageboard
 
         protected void LinkButton1_Click(object sender, EventArgs e)
         {
-            string uid = Request.QueryString["uid"] ?? Session["userid"]?.ToString();
+            // 只用Session中的userid
+            string uid = Session["userid"]?.ToString();
             if (string.IsNullOrEmpty(uid))
             {
                 Response.Redirect("login.aspx");
